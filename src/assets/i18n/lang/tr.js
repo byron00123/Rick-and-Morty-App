01a62e0b011b692ec51959a86a15b2de88452ea4
byron00123 <omondibyron25@gmail.com>
2024-04-16 15:12:43 +0300

    :construction:add language translate

---
+++ src/assets/i18n/lang/tr.js
@@ -0,0 +1,25 @@
+export const tr = {
+    translation: {
+      homeNav: 'Nyumbani',
+      charactersNav: 'Wahusika',
+      episodesNav: 'Matukio',
+      locationsNav: 'Maeneo',
+      favoriteCharNav: 'Wahusika Pendwa',
+      homeExplanation: 'Pata tabia yako pendwa ya Rick na Morty...',
+      searchPlaceholder: 'Tafuta...',
+      sortText: 'Chaguo-msingi',
+      charLastLocation: 'Mahali pa mwisho kujulikana:',
+      cellName: 'Jina',
+      cellType: 'Aina',
+      cellDimension: 'Upana',
+      cellCreated: 'Imeundwa',
+      cellAllChar: 'Taarifa za Wahusika Wote',
+      cellCharContent: 'ANGALIA WAHUSIKA WOTE',
+      cellAirDate: 'Tarehe ya Hewa',
+      cellEpisode: 'Matukio',
+      notFound: 'HAIPATIKANI',
+      dialogCharTitle: 'WAHUSIKA WOTE',
+      dialogNoData: 'HAKUNA DATA ILIYOPATIKANA',
+    },
+  };
+  
